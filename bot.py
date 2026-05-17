d9b731339375ba83320256641d6b67fb051d002d
AutoLinkTest <zalmat.amine.mz@gmail.com>
2026-05-17 04:03:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot actif"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
 import time
 import requests
 import feedparser
